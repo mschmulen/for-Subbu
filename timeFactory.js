775ad9a350b0a6d9c5e85178001bf6c658b225e2
Matt Schmulen <matt.schmulen@gmail.com>
2014-04-02 12:16:59 -0700

    updated AddMinutes to not use DateTime

--- timeFactory.js
+++ timeFactory.js
@@ -1,7 +1,7 @@
 
 var timeFactory = exports = module.exports;
 
-timeFactory.useDateTime = true;
+timeFactory.useDateTime = false;
 
 /**
  * Add Mintes to a time string
@@ -37,12 +37,45 @@ function AddMinutesWithDateTime( timeString, add_minutes )
 	
 };//end addTime
 
-function AddMinutesWithoutDateTime( t, m )
+/**
+ * Add Mintes to a time string
+ * @timeString : [H]H:MM {AM|PM}
+ * @add_minutes : number in minutes to add
+ * @return string : AddMinutes ("9:13 AM", 10) would return "9:23 AM"
+ */
+function AddMinutesWithoutDateTime( timeString, add_minutes )
 {
 	var outText  = "-";
+	//console.log( "timeString = " + timeString + " ---- " +  "add_minutes = " + add_minutes );
 	
-	return AddMinutesWithDateTime( timeString, add_minutes );
-	return outText;	
+	if ( !timeString || !add_minutes ) { console.log( "invalid arguments"); return outText; }
+	
+	var splitOnSpace = timeString.split(" ");
+	if ( splitOnSpace.length < 2 ) { console.log( "invalid arguments"); return outText; }
+	
+	var isAM = splitOnSpace[1].toUpperCase() === "AM";
+	
+	var splitOnColon = splitOnSpace[0].split(":");
+	if ( splitOnColon.length < 2 ) { console.log( "invalid arguments"); return outText; }
+	
+	var hours = parseInt( splitOnColon[0]);
+	var minutes = parseInt( splitOnColon[1]);
+	
+	if ( isAM == false ){ hours = hours + 12;}
+	
+	var totalMinutes = hours * 60 + minutes + add_minutes;
+	
+	var finalHours = parseInt(totalMinutes / 60);
+	
+	var finalMinutes = totalMinutes % 60;
+	
+	var AM_PM = finalHours >= 12 ? 'PM' : 'AM';
+	
+	var hoursInAM_PM = finalHours >= 12 ? finalHours - 12 : finalHours;
+	
+	outText = hoursInAM_PM + ":" + finalMinutes + " " + AM_PM;
+	
+	return outText;
 }//end AddMinutesWithoutDateTime
 
 timeFactory.AddMinutes = function ( timeString, add_minutes )
